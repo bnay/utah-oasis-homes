fb755ff7de988606aa199494fc2be91bd69d96cb
Brian Nay <brian@utahoasishomes.com>
2026-07-14 15:19:59 -0600

    Improve technical SEO and metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "ignore", 
-  site: "https://utahoasishomes.com",   
+  trailingSlash: "ignore",
+  site: "https://utahoasishomes.com",
+  integrations: [sitemap()],
 });
-
 
